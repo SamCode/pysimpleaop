0333212700d47dfd4079080d7eecab3883fe072d
samcode <samson_xie@aol.com>
2014-01-14 18:04:17 -0500

    indentation fix
    converted tabs to spaces

--- example.py
+++ example.py
@@ -8,18 +8,18 @@ observer = Aspect()
 
 @observer.aop
 def f1(x, y):
-	"""A trivial addition function."""
-	return x + y
+    """A trivial addition function."""
+    return x + y
 
 @observer.aop
 def f2(x, y, z):
-	"""Another trivial addition function."""
-	return f1(f1(x, y), z)
+    """Another trivial addition function."""
+    return f1(f1(x, y), z)
 
 # Define pointcuts and advices.
 
 def log1():
-	print "addition function logged"
+    print "addition function logged"
 
 observer.pointcut("addition", [f1, f2])
 observer.advice(AFTER, "addition", log1)
@@ -27,4 +27,4 @@ observer.advice(AFTER, "addition", log1)
 # Write execution code.
 
 if __name__ == "__main__":
-	print f1(1, 2) * f2(1, 2, 3)
+    print f1(1, 2) * f2(1, 2, 3)
